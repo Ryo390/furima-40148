f9bd7af2f70261723f1c2a088164abe59064667f
Ryo390 <151626117+Ryo390@users.noreply.github.com>
2023-12-05 16:40:32 +0900

    turai

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+ 
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'PAYJP_SECRET_KEY' && password == '4df29baf7d93d21bbe8404ad28712dad'
+    end
+  end
 end
