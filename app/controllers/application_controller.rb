e14ccc0909ecec96b05903a762fd1db3c6a37c55
Ryo390 <151626117+Ryo390@users.noreply.github.com>
2023-12-05 18:30:03 +0900

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == 'PAYJP_SECRET_KEY' && password == '4df29baf7d93d21bbe8404ad28712dad'
     end
   end
 end
